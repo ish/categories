eb33ab9a42c0d32b043834c2c3224d3828accc36
Tim Parkin <tim.parkin@jdi-associates.com>
2009-03-28 16:47:41 +0000

    tweaked tests to work with changelog

--- categories/tests/test_categorish.py
+++ categories/tests/test_categorish.py
@@ -83,7 +83,7 @@ class Test(unittest.TestCase):
           ]
         }
 
-        categories.rename_path_segment(before['category'], 'a.x', 'a.N')
+        categories.rename_path_segment(before['category'], 'a.x', 'a.N', [])
         assert before == expected
 
     def test_find_and_replace_pathchanges(self):
@@ -190,7 +190,7 @@ class TestChanges(unittest.TestCase):
 
         data = { "category":[] }
 
-        actual = list(categories.apply_changes(before['category'], data['category'], facet, category, create))
+        actual, c = list(categories.apply_changes(before['category'], data['category'], facet, category, create))
         print 'ACTUAL'
         print actual
         print 'EXPECTED'
@@ -223,7 +223,7 @@ class TestChanges(unittest.TestCase):
         data = { "category":[{ "id":"3", "path":"N", "data": "8", "new_category": {"is_new": False, "label": ""} },] }
 
 
-        actual = list(categories.apply_changes(before['category'], data['category'], facet, category, create))
+        actual, c = list(categories.apply_changes(before['category'], data['category'], facet, category, create))
         print 'ACTUAL'
         print actual
         print 'EXPECTED'
@@ -255,7 +255,7 @@ class TestChanges(unittest.TestCase):
         data = { "category":[{ "id":"2", "path":"N", "data": "7", "new_category": {"is_new": False, "label": ""} },] }
 
 
-        actual = list(categories.apply_changes(before['category'], data['category'], facet, category, create))
+        actual, c = list(categories.apply_changes(before['category'], data['category'], facet, category, create))
         print 'ACTUAL'
         print actual
         print 'EXPECTED'
@@ -287,7 +287,7 @@ class TestChanges(unittest.TestCase):
         data = { "category":[{ "id":"3", "path":"x", "data": None, "new_category": {"is_new": True, "label": "FooBar"} },] }
 
 
-        actual = list(categories.apply_changes(before['category'], data['category'], facet, category, create))
+        actual, c = list(categories.apply_changes(before['category'], data['category'], facet, category, create))
         print 'ACTUAL'
         print actual
         print 'EXPECTED'
@@ -319,7 +319,7 @@ class TestChanges(unittest.TestCase):
         data = { "category":[{ "id":"3", "path":"N", "data": None, "new_category": {"is_new": True, "label": "FooBar"} },] }
 
 
-        actual = list(categories.apply_changes(before['category'], data['category'], facet, category, create))
+        actual, c = list(categories.apply_changes(before['category'], data['category'], facet, category, create))
         print 'ACTUAL'
         print actual
         print 'EXPECTED'
@@ -353,7 +353,7 @@ class TestChanges(unittest.TestCase):
         data = { "category":[{ "id":"5", "path":"N", "data": "10", "new_category": {"is_new": False, "label": None} },{ "id":"3", "path":"x", "data": "8", "new_category": {"is_new": False, "label": None} }] }
 
 
-        actual = list(categories.apply_changes(before['category'], data['category'], facet, category, create))
+        actual, c = list(categories.apply_changes(before['category'], data['category'], facet, category, create))
         print 'ACTUAL'
         print actual
         print 'EXPECTED'
@@ -386,7 +386,7 @@ class TestChanges(unittest.TestCase):
           'path': u'france'}]}
 
        
-        actual = list(categories.apply_changes(before['category'], data['category'], facet, category, create))
+        actual, c = list(categories.apply_changes(before['category'], data['category'], facet, category, create))
         expected['category'][1]['id'] = actual[1]['id']
         print 'ACTUAL'
         print actual
@@ -423,7 +423,7 @@ class TestChanges(unittest.TestCase):
           'path': u'ireland'}]}
 
        
-        actual = list(categories.apply_changes(before['category'], data['category'], facet, category, create))
+        actual, c = list(categories.apply_changes(before['category'], data['category'], facet, category, create))
         expected['category'][2]['id'] = actual[2]['id']
         print 'ACTUAL'
         print actual
@@ -456,7 +456,7 @@ class TestChanges(unittest.TestCase):
         data = { "category":[{ "id":"3", "path":"x", "data": "9", "new_category": {"is_new": False, "label": None} },] }
 
 
-        actual = list(categories.apply_changes(before['category'], data['category'], facet, category, create))
+        actual, c = list(categories.apply_changes(before['category'], data['category'], facet, category, create))
         print 'ACTUAL'
         print actual
         print 'EXPECTED'
